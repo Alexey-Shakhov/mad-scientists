d8572167eddcd8ee7696b7cee9ce33700a95fcd1
Alexey Shakhov <alexey0shakhov@gmail.com>
2020-02-15 17:12:38 +0300

    Simplified tests

--- src/server/main_spec.rb
+++ src/server/main_spec.rb
@@ -5,7 +5,18 @@ require 'rack/test'
 require 'sequel'
 Sequel.extension :migration
 
-RSpec.describe "Crazy Scientists web-service" do
+RSpec.shared_examples "a dataset" do |field, values, path|
+  it "returns records with #{field} = #{values.join(", ")}" do
+    get path
+    result = JSON.parse(last_response.body)
+    values.each do |value|
+      filtered = result.select { |rec| rec[field] == value }
+      expect(filtered.length).to be > 0
+    end
+  end
+end
+
+RSpec.describe "Mad Scientists web-service" do
   include Rack::Test::Methods
 
   $db = Sequel.sqlite
@@ -16,69 +27,39 @@ RSpec.describe "Crazy Scientists web-service" do
   before :each do
     Device.dataset.destroy
     Scientist.dataset.destroy
+
+    Scientist.create(name: "Richard Feynman", madness_level: 5,
+                     galaxy_destruction_attempts: 0)
+    Scientist.create(name: "Frankenstein", madness_level: 11,
+                     galaxy_destruction_attempts: 0)
+    Scientist.create(name: "Emmett Brown", madness_level: 7,
+                     galaxy_destruction_attempts: 0)
+
+    Device.create(
+        name: "Atomic bomb",
+        scientist_id: Scientist.dataset[name: "Richard Feynman"].scientist_id,
+        power: 6)
+    Device.create(
+        name: "DeLorean time machine",
+        scientist_id: Scientist.dataset[name: "Emmett Brown"].scientist_id,
+        power: 0)
   end
 
   def app
     Sinatra::Application
   end
 
-  def setup_example_db
-    Scientist.create(name: "Richard Feynman", madness_level: 5,
-                     galaxy_destruction_attempts: 0)
-    Device.create(
-        name: "Atomic bomb",
-        scientist_id: Scientist.dataset[name: "Richard Feynman"].scientist_id,
-        power: 6)
-  end
-
   describe "#get '/scientists'" do
-    context "when the scientists table is not empty" do
-      it "returns the list of scientists as a JSON array of hashes" do
-        setup_example_db
-
-        get '/scientists'
-        result = JSON.parse(last_response.body)
-
-        expect(result[0]["name"]).to eq "Richard Feynman" 
-        expect(result[0]["madness_level"]).to eq 5 
-        expect(result[0]["galaxy_destruction_attempts"]).to eq 0 
-        expect(Time.now - Time.parse(result[0]["time_added"])).to be < 10
-      end
-    end
-
-    context "when the scientists table is empty" do
-      it "returns an empty array" do
-        get '/scientists'
-        result = JSON.parse(last_response.body)
-
-        expect(result).to eq [] 
-      end
+    context "when there are no filters" do
+      it_behaves_like "a dataset", "name",
+        ["Richard Feynman", "Emmett Brown", "Frankenstein"], '/scientists'
     end
   end
 
   describe "#get '/devices'" do
-    context "when the devices table is not empty" do
-      it "returns the list of devices as a JSON array of hashes" do
-        setup_example_db
-
-        get '/devices'
-        result = JSON.parse(last_response.body)
-
-        expect(result[0]["name"]).to eq "Atomic bomb" 
-        expect(result[0]["power"]).to eq 6 
-        scientist = result[0]["scientist_id"]
-        expect(Scientist[scientist_id: scientist].name).to eq "Richard Feynman" 
-        expect(Time.now - Time.parse(result[0]["time_added"])).to be < 10
-      end
-    end
-
-    context "when the devices table is empty" do
-      it "returns an empty array" do
-        get '/devices'
-        result = JSON.parse(last_response.body)
-
-        expect(result).to eq [] 
-      end
+    context "when there are no filters" do
+      it_behaves_like "a dataset", "name",
+        ["DeLorean time machine", "Atomic bomb"], '/devices'
     end
   end
 end
