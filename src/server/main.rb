d7b68809624b776fededd87d59cb3d7e651c34dd
Alexey Shakhov <alexey0shakhov@gmail.com>
2020-02-17 14:11:29 +0300

    Added spec and implementation for post '/scientists'

--- src/server/main.rb
+++ src/server/main.rb
@@ -1,5 +1,7 @@
 require 'sinatra'
 require 'sequel'
+require 'net/http'
+require 'set'
 Sequel::Model.plugin :json_serializer
 
 # The $db variable needs to be initialized before requiring this module
@@ -20,6 +22,53 @@ get '/scientists' do
   Scientist.all.to_json
 end
 
+get '/scientists/:id' do |id|
+  Scientist[scientist_id: id.to_i].to_json
+end
+
 get '/devices' do
   Device.all.to_json
+end
+
+post '/scientists' do
+  begin
+    records = JSON.parse request.body.read.to_s
+  rescue JSON::ParserError
+    halt 400, "failed to parse JSON"
+  end
+
+  if records.class != Array
+    halt 400, "invalid request body format"
+  end
+
+  fields = {"name": String, "madness_level": Integer,
+            "galaxy_destruction_attempts": Integer}
+
+  records.each do |rec|
+    if rec.class != Hash
+      halt 400, "invalid request body format"
+    end
+
+    # Check that there are no missing or redundant keys in the record
+    if rec.size != fields.size
+      halt 400, "invalid request body format"
+    end
+    rec.keys.each do |key|
+      if not fields.keys.include? key.to_sym
+        halt 400, "invalid request body format"
+      end
+    end
+
+    rec.keys.each do |key|
+      if rec[key].class != fields[key.to_sym]
+        halt 400, "invalid request body format"
+      end
+    end
+  end
+
+  records.each do |rec|
+    Scientist.create(rec)
+  end
+
+  status 204
 end
--- src/server/main_spec.rb
+++ src/server/main_spec.rb
@@ -16,17 +16,25 @@ RSpec.shared_examples "a dataset" do |field, values, path|
   end
 end
 
+RSpec.shared_examples "a single record" do |field, value, path|
+  it "returns the record with #{field} = #{value}" do
+    get path
+    result = JSON.parse(last_response.body)
+    expect(result[field]).to eq value
+  end
+end
+
 RSpec.describe "Mad Scientists web-service" do
   include Rack::Test::Methods
 
-  $db = Sequel.sqlite
+  $db = Sequel.sqlite(cache_schema: false)
   Sequel::Migrator.apply($db, '../migrations')
 
   require_relative 'main'
 
   before :each do
-    Device.dataset.destroy
-    Scientist.dataset.destroy
+    Device.dataset.truncate
+    Scientist.dataset.truncate
 
     Scientist.create(name: "Richard Feynman", madness_level: 5,
                      galaxy_destruction_attempts: 0)
@@ -56,11 +64,141 @@ RSpec.describe "Mad Scientists web-service" do
     end
   end
 
+  describe "#get '/scientists/:id'" do
+#    it_behaves_like "a single record", "scientist_id", 1, '/scientists/1'
+  end
+
   describe "#get '/devices'" do
     context "when there are no filters" do
       it_behaves_like "a dataset", "name",
         ["DeLorean time machine", "Atomic bomb"], '/devices'
     end
   end
+
+  describe "#post '/scientists'" do
+    context "when every record in the array has all the necessary fields" +
+      " and no redundant ones" do
+      it "adds new records" do
+        data = [
+          {
+            'name' => "One",
+            'madness_level' => 10,
+            'galaxy_destruction_attempts' => 12,
+          },
+          {
+            'name' => "Two",
+            'madness_level' => 80,
+            'galaxy_destruction_attempts' => 1024,
+          }
+        ]
+
+        post '/scientists', data.to_json
+
+        expect(last_response.status).to eq 204
+        
+        get '/scientists'
+        result = JSON.parse last_response.body
+
+        expect(result[-2]["name"]).to eq "One"
+        expect(result[-2]["madness_level"]).to eq 10
+        expect(result[-2]["galaxy_destruction_attempts"]).to eq 12
+
+        expect(result[-1]["name"]).to eq "Two"
+        expect(result[-1]["madness_level"]).to eq 80
+        expect(result[-1]["galaxy_destruction_attempts"]).to eq 1024
+      end
+    end
+
+    context "when invalid JSON is sent" do
+      context "when failed to parse JSON" do
+        it "returns 400 code with 'failed to parse JSON' message" do
+          post '/scientists', "[{dkjghk: 10, dfgf}]"
+          expect(last_response.status).to eq 400
+          expect(last_response.body).to eq "failed to parse JSON"
+        end
+      end
+
+      context "when sent JSON is not an array of hashes" do
+        context "when sent data is not an array" do
+          it "returns code 400 with 'invalid request body format' message" do
+            post '/scientists', {"koo" => 123}.to_json
+
+            expect(last_response.status).to eq 400
+            expect(last_response.body).to eq 'invalid request body format'
+          end
+        end
+
+        context "when the array contains a non-hash element" do
+          it "returns code 400 with 'invalid request body format' message" do
+            post '/scientists', [{}, {}, [], {}].to_json
+
+            expect(last_response.status).to eq 400
+            expect(last_response.body).to eq 'invalid request body format'
+          end
+        end
+      end
+
+      context "when one of the hashes contains a non-string key" do
+        it "returns code 400 with 'invalid request body format' message" do
+          post '/scientists', [{"name" => "One", "madness_level" => 6,
+                                10 => 1}].to_json
+
+          expect(last_response.status).to eq 400
+          expect(last_response.body).to eq 'invalid request body format'
+        end
+      end
+
+      context "when there is a missing field in one of the records" do
+        it "returns code 400 with 'invalid request body format' message" do
+          data = [
+            {
+              "name" => "One",
+              "madness_level" => 10
+            }
+          ]
+          post '/scientists', data.to_json
+
+          expect(last_response.status).to eq 400
+          expect(last_response.body).to eq 'invalid request body format'
+        end
+      end
+
+      context "when there is a redundant field in one of the records" do
+        it "returns code 400 with 'invalid request body format' message" do
+          data = [
+            {
+              "name" => "One",
+              "madness_level" => 10,
+              "galaxy_destruction_attempts" => 8,
+              "height" => 180,
+            }
+          ]
+          post '/scientists', data.to_json
+
+          expect(last_response.status).to eq 400
+          expect(last_response.body).to eq 'invalid request body format'
+        end
+      end
+
+      context "when a record has invalid data types" do
+        it "returns code 400 with 'invalid request body format' message" do
+          data = [
+            {
+              "name" => "One",
+              "madness_level" => 10,
+              "galaxy_destruction_attempts" => "eight"
+            }
+          ]
+
+          post '/scientists', data.to_json
+
+          expect(last_response.status).to eq 400
+          expect(last_response.body).to eq 'invalid request body format'
+        end
+      end
+    end
+  end
 end
+
+
 
